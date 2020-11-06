dfea9c405b11f951e633bebb867ed262105a7d1d
Ken <ludiazken@gmail.com>
2020-11-06 15:45:51 -0500

    Added styling to loginform.

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,6 +2,13 @@ import React, { useContext } from 'react';
 import Inputs from './Inputs';
 import ContinueAsGuestBtn from './ContinueAsGuestBtn';
 import {UserContext } from '../Contexts/UserContext';
+import styled from 'styled-components';
+
+const LogInForm = styled.div`
+    display: flex;
+    justify-content: center;
+    allign-items: center;
+`
 
 function LoginForm(props) {
     const [loggedIn, toggleLogIn] = useContext(UserContext);
